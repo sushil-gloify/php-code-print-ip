8c8f580b63265400b18daff93191cf53127a2884
sushil-gloify <102666277+sushil-gloify@users.noreply.github.com>
2023-01-12 17:30:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,3 +7,17 @@ print("\n");
 print `ifconfig`;
 
 ?>
+
+
+
+
+<?php
+#echo '<pre>';
+print("Welcome to Php");
+
+#print("\n");
+
+#print_r (`ifconfig`);
+#print_r(explode(`ifconfig`));
+echo $_SERVER['SERVER_ADDR'];
+?>
